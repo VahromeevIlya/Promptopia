74c73d4348bc756a56a1045cb5f7a41fb9dacf65
vahromeevilya-widestudio <98152955+vahromeevilya-widestudio@users.noreply.github.com>
2023-09-04 12:21:51 +0300

    hide email

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -32,7 +32,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 							{post.creator.username}
 						</h3>
 						<p className="font-inter text-sm text-gray-500">
-							{post.creator.email}
+							secretmail@gmail.com
 						</p>
 					</div>
 				</Link>
